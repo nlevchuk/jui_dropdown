37a3a043205332844d8dc5b623dc928a250f1b0f
Nikita Levchuk <nikitalevchuk@gmail.com>
2013-07-22 16:16:15 +0700

    modified plugin for work only with multiple elements

--- jquery.jui_dropdown.js
+++ jquery.jui_dropdown.js
@@ -47,101 +47,98 @@
 
             var elem = this;
 
-            return this.each(function() {
+            /**
+             * settings and defaults
+             * using $.extend, settings modification will affect elem.data() and vive versa
+             */
+            var settings = elem.data(pluginName);
+            if(typeof(settings) == 'undefined') {
+                var defaults = elem.jui_dropdown('getDefaults');
+                settings = $.extend({}, defaults, options);
+            } else {
+                settings = $.extend({}, settings, options);
+            }
+            elem.data(pluginName, settings);
 
-                /**
-                 * settings and defaults
-                 * using $.extend, settings modification will affect elem.data() and vive versa
-                 */
-                var settings = elem.data(pluginName);
-                if(typeof(settings) == 'undefined') {
-                    var defaults = elem.jui_dropdown('getDefaults');
-                    settings = $.extend({}, defaults, options);
-                } else {
-                    settings = $.extend({}, settings, options);
-                }
-                elem.data(pluginName, settings);
+            // bind events
+            elem.unbind("onSelect").bind("onSelect", settings.onSelect);
 
-                // bind events
-                elem.unbind("onSelect").bind("onSelect", settings.onSelect);
+            elem.removeClass().addClass(settings.containerClass);
 
-                elem.removeClass().addClass(settings.containerClass);
+            var launcher_id = settings.launcher_id;
+            var launcher_container_id = settings.launcher_container_id;
+            var menu_id = settings.menu_id;
+            var elem_launcher = $(this).find("." + launcher_id);
+            var elem_launcher_container = $(this).find("." + launcher_container_id);
+            var elem_menu = $(this).find("." + menu_id);
 
-                var launcher_id = settings.launcher_id;
-                var launcher_container_id = settings.launcher_container_id;
-                var menu_id = settings.menu_id;
-                var elem_launcher = $("#" + launcher_id);
-                var elem_launcher_container = $("#" + launcher_container_id);
-                var elem_menu = $("#" + menu_id);
+            elem_launcher_container.removeClass(settings.launcherContainerClass).addClass(settings.launcherContainerClass);
+            elem_launcher.removeClass(settings.launcherClass).addClass(settings.launcherClass);
+            elem_menu.removeClass(settings.menuClass).addClass(settings.menuClass);
 
-                elem_launcher_container.removeClass(settings.launcherContainerClass).addClass(settings.launcherContainerClass);
-                elem_launcher.removeClass(settings.launcherClass).addClass(settings.launcherClass);
-                elem_menu.removeClass(settings.menuClass).addClass(settings.menuClass);
+            if(encode_version($.ui.version) < encode_version('1.9.0')) {
+                elem_menu.menu().menu('refresh').hide();
 
-                if(encode_version($.ui.version) < encode_version('1.9.0')) {
-                    elem_menu.menu().menu('refresh').hide();
-
-                    elem_menu.off('click', "li").on('click', "li", function() {
-                        elem.triggerHandler('onSelect', {index: parseInt($(this).index("#" + menu_id + " li")) + 1, id: $(this).attr("id")})
-                    });
-                } else {
-                    elem_menu.menu({
-                        select: function(event, ui) {
-                            elem.triggerHandler('onSelect', {index: parseInt(ui.item.index("#" + menu_id + " li")) + 1, id: ui.item.attr("id")})
-                        }
-                    }).menu('refresh').hide();
-                }
-
-                if(settings.launcher_is_UI_button) {
-                    elem_launcher.button({
-                        text: settings.launcherUIShowText,
-                        icons: {
-                            primary: settings.launcherUIPrimaryIconClass,
-                            secondary: settings.launcherUISecondaryIconClass
-                        }
-                    });
-                }
-
-                elem.off('click', "#" + launcher_id).on('click', "#" + launcher_id, function() {
-
-                    var jui_dropdown_current_menu_id = $(document).data("jui_dropdown_current_menu_id");
-                    if(typeof(jui_dropdown_current_menu_id) != 'undefined') {
-                        $("#" + jui_dropdown_current_menu_id).hide();
-                    }
-
-                    if(!settings.launcher_is_UI_button && settings.toggle_launcher) {
-                        elem_launcher.addClass(settings.launcherSelectedClass);
-                    }
-
-                    elem_menu.show().position({
-                        my: settings.my_position,
-                        at: settings.at_position,
-                        of: elem_launcher_container
-                    });
-
-                    $(document).one("click", function() {
-                        elem_menu.hide();
-                        if(!settings.launcher_is_UI_button && settings.toggle_launcher) {
-                            elem_launcher.removeClass(settings.launcherSelectedClass);
-                        }
-                    });
-
-                    $(document).data("jui_dropdown_current_menu_id", menu_id);
-
-                    return false;
+                elem_menu.off('click', "li").on('click', "li", function() {
+                    elem.triggerHandler('onSelect', {index: parseInt($(this).index("." + menu_id + " li")) + 1, id: $(this).attr("id")})
                 });
+            } else {
+                elem_menu.menu({
+                    select: function(event, ui) {
+                        elem.triggerHandler('onSelect', {index: parseInt(ui.item.index("." + menu_id + " li")) + 1, id: ui.item.attr("id")})
+                    }
+                }).menu('refresh').hide();
+            }
 
-                elem.off('mouseenter', "#" + launcher_id).on('mouseenter', "#" + launcher_id, function() {
-                    if(settings.launchOnMouseEnter) {
-                        elem_launcher.trigger('click');
+            if(settings.launcher_is_UI_button) {
+                elem_launcher.button({
+                    text: settings.launcherUIShowText,
+                    icons: {
+                        primary: settings.launcherUIPrimaryIconClass,
+                        secondary: settings.launcherUISecondaryIconClass
                     }
                 });
+            }
 
-                elem.off('mouseleave', "#" + menu_id).on('mouseleave', "#" + menu_id, function() {
+            elem.off('click', "." + launcher_id).on('click', "." + launcher_id, function() {
+
+                var jui_dropdown_current_menu_id = $(document).data("jui_dropdown_current_menu_id");
+                if(typeof(jui_dropdown_current_menu_id) != 'undefined') {
+                    $("." + jui_dropdown_current_menu_id).hide();
+                }
+
+                if(!settings.launcher_is_UI_button && settings.toggle_launcher) {
+                    elem_launcher.addClass(settings.launcherSelectedClass);
+                }
+
+                elem_menu.show().position({
+                    my: settings.my_position,
+                    at: settings.at_position,
+                    of: elem_launcher_container
+                });
+
+                $(document).one("click", function() {
                     elem_menu.hide();
+                    if(!settings.launcher_is_UI_button && settings.toggle_launcher) {
+                        elem_launcher.removeClass(settings.launcherSelectedClass);
+                    }
                 });
 
+                $(document).data("jui_dropdown_current_menu_id", menu_id);
+
+                return false;
             });
+
+            elem.off('mouseenter', "." + launcher_id).on('mouseenter', "." + launcher_id, function() {
+                if(settings.launchOnMouseEnter) {
+                    elem_launcher.trigger('click');
+                }
+            });
+
+            elem.off('mouseleave', "." + menu_id).on('mouseleave', "." + menu_id, function() {
+                elem_menu.hide();
+            });
+
 
         },
 
@@ -281,11 +278,13 @@
      */
     $.fn.jui_dropdown = function(method) {
 
+        /*
         if(this.size() != 1) {
             var err_msg = 'You must use this plugin (' + pluginName + ') with a unique element (at once)';
             this.html('<span style="color: red;">' + 'ERROR: ' + err_msg + '</span>');
             $.error(err_msg);
         }
+        */
 
         // Method calling logic
         if(methods[method]) {
